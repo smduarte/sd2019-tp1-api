4bd88425aed43105cd1d6e39939eb4513f71ab57
smd <smd@fct.unl.pt>
2019-04-15 14:03:47 +0100

    Fixed missing @Consumes in isLiked operation...

--- SD2019-Microgram-API/src/microgram/api/rest/RestPosts.java
+++ SD2019-Microgram-API/src/microgram/api/rest/RestPosts.java
@@ -15,46 +15,47 @@ import javax.ws.rs.core.MediaType;
 import microgram.api.Post;
 
 /**
- * REST API of the Posts service 
+ * REST API of the Posts service
  * 
  * @author smd
  *
  */
-@Path( RestPosts.PATH )
+@Path(RestPosts.PATH)
 public interface RestPosts {
 
-static final String PATH="/posts";
-	
+	static final String PATH = "/posts";
+
 	@GET
 	@Path("/{postId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	Post getPost( @PathParam("postId") String postId );
-		
+	Post getPost(@PathParam("postId") String postId);
+
 	@DELETE
 	@Path("/{postId}")
-	void deletePost( @PathParam("postId") String postId );
-	
+	void deletePost(@PathParam("postId") String postId);
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	String createPost( Post post );
+	String createPost(Post post);
 
 	@GET
 	@Path("/{postId}/likes/{userId}")
-	boolean isLiked( @PathParam("postId") String postId, @PathParam("userId") String userId);
+	@Produces(MediaType.APPLICATION_JSON)
+	boolean isLiked(@PathParam("postId") String postId, @PathParam("userId") String userId);
 
 	@PUT
-	@Path("/{postId}/likes/{userId}")	
+	@Path("/{postId}/likes/{userId}")
 	@Consumes(MediaType.APPLICATION_JSON)
-	void like( @PathParam("postId") String postId, @PathParam("userId") String userId, boolean isLiked);
-		
+	void like(@PathParam("postId") String postId, @PathParam("userId") String userId, boolean isLiked);
+
 	@GET
 	@Path("/from/{userId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	List<String> getPosts( @PathParam("userId") String userId);
-	
+	List<String> getPosts(@PathParam("userId") String userId);
+
 	@GET
 	@Path("/feed/{userId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	List<String> getFeed( @PathParam("userId") String userId);
+	List<String> getFeed(@PathParam("userId") String userId);
 }
